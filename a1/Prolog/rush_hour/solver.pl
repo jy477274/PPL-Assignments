e56e1c8bc37917509672890728692afd21512fc4
Al Brink <AlBrink@T92A3.WPA.Dal.Ca>
2019-02-12 14:56:21 -0400

    added comments to prolog file containing general solution algo using for loops

--- a1/Prolog/rush_hour/solver.pl
+++ a1/Prolog/rush_hour/solver.pl
@@ -25,8 +25,22 @@ puzzle_solution(Puzzle, Solution) :-
   
 solve_puzzle(State, Solution) :-
 
-
-  
+%for(i=0; i<64, i++)
+% state_is_horizontal(State, i)
+%  state_is_end(State, i)
+%	for(j=1, j<5, j++)
+%    horizontal_move(State, i, j, NewState)  -- on success, where do we put NewState (in _next and _seen list?)
+%	for(j=-1, j>-5, j--)
+%	 horizontal_move(State, i, j, NewState)
+% state_is_vertical(State, i)
+%  state_is_end(State, i)
+%	for(j=1, j<5, j++)
+%    vertical_move(State, i, j, NewState)  -- on success, where do we put NewState (in _next and _seen list?)
+%	for(j=-1, j>-5, j--)
+%	 vertical_move(State, i, j, NewState)
+% check current/next list using state_is_solved(State) to look for solution - print sol and exit if true
+% else call solve_puzzle with state in _next and set _current list to _next list and wipe _next
+% 
 
 % Solve the puzzle
 %solve_puzzle(Puzzle, Moves).
