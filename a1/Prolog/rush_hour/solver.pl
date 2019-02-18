e8541b2f5e7aa22c64b392e9ef1039942674e213
Dylan Roberts <dylanroberts@dal.ca>
2019-02-18 19:27:07 -0400

    changed some stuff check the logic

--- a1/Prolog/rush_hour/solver.pl
+++ a1/Prolog/rush_hour/solver.pl
@@ -20,32 +20,31 @@ File Edit Options Buffers Tools Help
 % IMPLEMENT THE FOLLOWING PREDICATE
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-puzzle_solution(Puzzle, Solution) :-
-  puzzle_state(Puzzle, State),
-  once(solve_puzzle(State, Solution)).
+solve_puzzle(Puzzle, Moves) :-
+    puzzle_state(Puzzle, State0),
+    once(move_list_gen(State0, Moves)).
 
 
-solve_puzzle(State, []):-
-    state_is_solved(State).
+% When a solution is found builds an empty list of move objects
+move_list_gen(State, [ ]) :-
+    state_is_solved(State),!.
 
 
-solve_puzzle(State, [M|Moves]):-
-    find_cars(State, Cars),
-    between(-4, 4, Offset), Offset \= 0,
-    pos_offset_move(Cars, Offset, M),
-    setof(M, gen_new_states(State, Cars, Offset, States), States),
-    solve_puzzle(States, Moves).
+move_list_gen(StateN, [MoveN|Moves]) :-
+    between(-4, 4, Offset), Offset #\= 0,
+    car_pos(StateN, CarPos),
+    gen_new_states(StateN, CarPos, Offset, NewStates),
+    pos_offset_move(CarPos, Offset, MoveN),
+    move_list_gen(NewStates, Moves).
+
+car_pos(StateN, CarPos) :-
+    between(0, 63, X),
+    findall(X, state_is_end(StateN, X), CarPos).
 
 
-gen_new_states(State, Cars, Offset, States):-
-    horizontal_move(State, Cars, Offset, HStates),
-    vertical_move(State, Cars, Offset, VStates),
-    append(HStates,VStates, States).
-
-
-find_cars(State, Cars):-
-    between(0, 63, Pos),
-    findall(Pos, state_is_end(States, Pos), Cars).
+gen_new_states(StateN, CarPos, Offset, NewState) :-
+    horizontal_move(StateN, CarPos, Offset, NewState);
+    vertical_move(StateN, CarPos, Offset, NewState).
 
 
 % Solve the puzzle
